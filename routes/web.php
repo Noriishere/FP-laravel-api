39511b105f0e947eae40de180b6a35905f7ab8c3
Noriishere <noriyamashin@gmail.com>
2026-04-26 12:49:17 +0700

    Feat: Admin-Users

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::middleware(['auth:web', 'role:admin'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('/users', [UserController::class]);
+    Route::resource('users', UserController::class);
 });
 
 require __DIR__.'/auth.php';
