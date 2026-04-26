e85fbae2e62916bcf740f6c3e49fdd895bfbb164
Noriishere <noriyamashin@gmail.com>
2026-04-26 12:00:00 +0700

    Feat: Sidebar Refinement

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,8 @@ Route::get('/', function () {
     return view('pages.home');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth:api', 'role:admin'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
