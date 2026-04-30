1ddbe639380277c54eadfbc2ff5a47bbacc6a94d
Noriishere <noriyamashin@gmail.com>
2026-04-30 23:52:04 +0700

    Feat: add routes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -135,6 +135,13 @@ class DatabaseSeeder extends Seeder
             'role' => 'admin',
             'email_verified_at' => now()
         ]);
+        User::create([
+            'name' => 'Asep',
+            'email' => 'asep@gmail.com',
+            'password' => 'admin123',
+            'role' => 'customer',
+            'email_verified_at' => now()
+        ]);
         Driver::factory()->count(3)->create();
         Vehicle::factory()->count(2)->create();
         $routeIds = Route::has('stops')->pluck('id');
