4d12bb3bd2e996f0bfac3ff157d67cf590b18999
Noriishere <noriyamashin@gmail.com>
2026-05-08 18:43:05 +0700

    Feat: perubahan besar

--- app/Models/BookingSeat.php
+++ app/Models/BookingSeat.php
@@ -2,15 +2,37 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BookingSeat extends Model
 {
-    use HasFactory;
     protected $fillable = [
-        'schedule_id',
+
         'booking_id',
-        'seat_id'
+
+        'seat_id',
+
+        'schedule_id',
     ];
+
+    public function booking()
+    {
+        return $this->belongsTo(
+            Booking::class
+        );
+    }
+
+    public function seat()
+    {
+        return $this->belongsTo(
+            Seat::class
+        );
+    }
+
+    public function schedule()
+    {
+        return $this->belongsTo(
+            Schedule::class
+        );
+    }
 }
