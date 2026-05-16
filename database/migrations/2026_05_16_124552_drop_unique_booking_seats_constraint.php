420a57938df297f2e09d03afafd95206688e0ddc
Noriishere <noriyamashin@gmail.com>
2026-05-16 19:51:06 +0700

    Feat: Hadeuh

--- database/migrations/2026_05_16_124552_drop_unique_booking_seats_constraint.php
+++ database/migrations/2026_05_16_124552_drop_unique_booking_seats_constraint.php
@@ -9,19 +9,38 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('booking_seats', function (Blueprint $table) {
+
+            $table->dropForeign(['schedule_id']);
+
             $table->dropUnique(
                 'booking_seats_schedule_id_seat_id_unique'
             );
+        });
+
+        Schema::table('booking_seats', function (Blueprint $table) {
+
+            $table->foreign('schedule_id')
+                ->references('id')
+                ->on('schedules')
+                ->cascadeOnDelete();
         });
     }
 
     public function down(): void
     {
         Schema::table('booking_seats', function (Blueprint $table) {
+
+            $table->dropForeign(['schedule_id']);
+
             $table->unique([
                 'schedule_id',
                 'seat_id'
             ]);
+
+            $table->foreign('schedule_id')
+                ->references('id')
+                ->on('schedules')
+                ->cascadeOnDelete();
         });
     }
 };
