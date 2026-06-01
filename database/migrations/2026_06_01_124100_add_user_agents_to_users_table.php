ac1c27bd5ba6f03dfa02a9cfe715b64d8b5a5a48
Noriishere <noriyamashin@gmail.com>
2026-06-01 12:42:18 +0700

    Feat: add user agents

---
+++ database/migrations/2026_06_01_124100_add_user_agents_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('last_user_agent')->nullable();
+            $table->string('last_ip_address', 45)->nullable();
+            $table->timestamp('last_login_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
